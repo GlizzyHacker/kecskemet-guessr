7968813aaa28d6cda4eee52fbc01de0707ce7420
Attila Sax <saxattila@gmail.com>
2025-07-03 18:28:10 +0200

    map zooms to guesses

--- apps/frontend/src/components/map.tsx
+++ apps/frontend/src/components/map.tsx
@@ -4,7 +4,9 @@ import { LatLng } from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { GuessMarker } from './guess_marker';
+import LocationMarker from './location_marker';
 
 export default function Map(options: {
   location: ParsedCordinates | null;
@@ -24,35 +26,14 @@ export default function Map(options: {
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <GuessMarker onMapClick={options.onMapClick} guess={options.guess}></GuessMarker>
-      {!options.location ? null : (
-        <Marker opacity={0.5} position={[options.location!.lat, options.location!.lng]}>
-          <Popup>Image Location</Popup>
-        </Marker>
-      )}
+      <LocationMarker location={options.location} bounds={options.guesses.map((e) => e.latLng)} />
       {options.guesses?.map((e) => (
         <Marker key={e.player.id} position={[e.latLng.lat, e.latLng.lng]}>
           <Popup>
-            {e.player.name}'s Guess score:{e.score}
+            {e.player.name}'s guess score:{e.score}
           </Popup>
         </Marker>
       ))}
     </MapContainer>
-  );
-}
-
-export function GuessMarker(options: { onMapClick: (point: LatLng) => void; guess: ParsedCordinates | null }) {
-  const map = useMapEvents({
-    click: (e) => {
-      options.onMapClick(e.latlng);
-    },
-  });
-  return (
-    <>
-      {options.guess == null ? null : (
-        <Marker position={[options.guess.lat, options.guess.lng]}>
-          <Popup>My guess</Popup>
-        </Marker>
-      )}
-    </>
   );
 }
