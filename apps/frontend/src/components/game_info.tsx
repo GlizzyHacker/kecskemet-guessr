b51cd3a34f47655148ed74e7bcae15f81fda6735
Attila Sax <saxattila@gmail.com>
2025-07-07 19:32:56 +0200

    show game options

--- apps/frontend/src/components/game_info.tsx
+++ apps/frontend/src/components/game_info.tsx
@@ -1,11 +1,18 @@
-import { Game } from '@/types/game';
+import { Area, Difficulty, Game } from '@/types/game';
 import Card from './card';
 
 export default function GameInfo({ className = '', game }: { className?: string; game: Game }) {
   return (
-    <Card className={`${className} flex flex-row`}>
-      <p className='p-2'>Game id: {game.id}</p>
-      <p className='p-2'>Round: {game.round}</p>
+    <Card className={`${className}`}>
+      <h1 className='text-center p-3'>
+        Round {game.round} out of {game.totalRounds}
+      </h1>
+      <div className=''>
+        <p className='p-2'>Game id: {game.id}</p>
+        <p className='p-2'>Difficulty: {game.difficulty}</p>
+        <p className='p-2'>Area: {game.area}</p>
+        <p className='p-2'>Szétütöm a fejed gaydácsi</p>
+      </div>
     </Card>
   );
 }
