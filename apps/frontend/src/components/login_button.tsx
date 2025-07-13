978a8aebd2d33a1993787995895f09bc2d0b07dc
Attila Sax <saxattila@gmail.com>
2025-07-13 11:58:23 +0200

    changed rounded corners

--- apps/frontend/src/components/login_button.tsx
+++ apps/frontend/src/components/login_button.tsx
@@ -2,22 +2,13 @@
 
 import usePlayer from '@/hooks/usePlayer';
 import Link from 'next/link';
-import Button from './button';
 
 export default function LoginButton() {
   const { data: player } = usePlayer();
 
-  if (player) {
-    return (
-      <Link className='text-white' href={'/player'}>
-        {player.name}
-      </Link>
-    );
-  }
-
   return (
-    <Link href={'/player'}>
-      <Button>Create player</Button>
+    <Link className='text-white' href={'/player'}>
+      {player ? player.name : 'Create player'}
     </Link>
   );
 }
