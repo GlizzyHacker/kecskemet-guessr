48887536fc48674e99ac39930d5f488167eb2b1b
Attila Sax <saxattila@gmail.com>
2025-07-19 11:19:58 +0200

    left out home page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Button from '@/components/button';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 export default function Home() {
+  const t = useTranslations('Home');
   return (
     <main>
       <div className='flex flex-row justify-center items-center'>
         <div>
           <Link href='/play'>
-            <Button>Play</Button>
+            <Button>{t('play')}</Button>
           </Link>
         </div>
       </div>
