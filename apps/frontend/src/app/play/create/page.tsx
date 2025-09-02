25b503692e40567dda1f0fecbe4a24391f982d23
Attila Sax <saxattila@gmail.com>
2025-09-02 13:01:35 +0200

    made create page responsive

--- apps/frontend/src/app/play/create/page.tsx
+++ apps/frontend/src/app/play/create/page.tsx
@@ -106,84 +106,86 @@ export default function Create() {
 
   return (
     <main className='flex flex-col items-5 min-w-2/3 mx-auto'>
-      <div className='flex flex-row bg-secondary rounded-xl p-2'>
-        <div className='flex-1'>
-          <Map areas={selectedAreas} />
-        </div>
-        <div className='flex-1 flex flex-col px-8 pb-8 items-center'>
-          <form className='grid grid-cols-2 space-y-2' action={handleForm}>
-            <h2 className='font-medium text-white transition px-4 pb-2 pt-4 col-span-2 mx-auto'>Create Game</h2>
-            <div>
-              <h2>{t('difficulty')}</h2>
-              <div className='pl-2'>
-                <Radio group='difficulty' value='EASY' isDefault>
-                  {t('easy')}
-                </Radio>
-                <Radio group='difficulty' value='NORMAL'>
-                  {t('normal')}
-                </Radio>
-                <Radio group='difficulty' value='HARD'>
-                  {t('hard')}
-                </Radio>
+      <div className='bg-secondary rounded-xl p-2'>
+        <div className='flex max-md:flex-col rounded-[10] overflow-hidden'>
+          <div className='flex-1'>
+            <Map areas={selectedAreas} />
+          </div>
+          <div className='flex-1 flex flex-col px-8 pb-8 items-center'>
+            <form className='grid grid-cols-2 space-y-2' action={handleForm}>
+              <h2 className='font-medium text-white transition px-4 pb-2 pt-4 col-span-2 mx-auto'>Create Game</h2>
+              <div className=' max-md:col-span-2'>
+                <h2>{t('difficulty')}</h2>
+                <div className='pl-2'>
+                  <Radio group='difficulty' value='EASY' isDefault>
+                    {t('easy')}
+                  </Radio>
+                  <Radio group='difficulty' value='NORMAL'>
+                    {t('normal')}
+                  </Radio>
+                  <Radio group='difficulty' value='HARD'>
+                    {t('hard')}
+                  </Radio>
+                </div>
               </div>
-            </div>
-            <div>
-              <h2>{t('area')}</h2>
-              <div className='pl-2'>
-                <Radio group='area' value='NORMAL' isDefault onSelect={() => onPresetArea('NORMAL')}>
-                  {t('grand_boulevard')}
-                </Radio>
-                <Radio group='area' value='EXPANDED' onSelect={() => onPresetArea('EXPANDED')}>
-                  {t('urban_areas')}
-                </Radio>
-                <Radio group='area' value='ALL' onSelect={() => onPresetArea('ALL')}>
-                  {t('all')}
-                </Radio>
-                <Radio group='area' value='OTHER' onSelect={onCustomArea}>
-                  {t('other')}
-                </Radio>
+              <div className=' max-md:col-span-2'>
+                <h2>{t('area')}</h2>
+                <div className='pl-2'>
+                  <Radio group='area' value='NORMAL' isDefault onSelect={() => onPresetArea('NORMAL')}>
+                    {t('grand_boulevard')}
+                  </Radio>
+                  <Radio group='area' value='EXPANDED' onSelect={() => onPresetArea('EXPANDED')}>
+                    {t('urban_areas')}
+                  </Radio>
+                  <Radio group='area' value='ALL' onSelect={() => onPresetArea('ALL')}>
+                    {t('all')}
+                  </Radio>
+                  <Radio group='area' value='OTHER' onSelect={onCustomArea}>
+                    {t('other')}
+                  </Radio>
+                </div>
               </div>
-            </div>
-            {showAreaSelection && areas && (
-              <div className='grid grid-cols-2 col-span-2 '>
-                <Checkboxes options={[...areas].map((area) => area[0])} selectionChanged={setSelectedAreas} />
+              {showAreaSelection && areas && (
+                <div className='grid grid-cols-2 col-span-2 '>
+                  <Checkboxes options={[...areas].map((area) => area[0])} selectionChanged={setSelectedAreas} />
+                </div>
+              )}
+              <div className=' max-md:col-span-2'>
+                <h2>{t('timer')}</h2>
+                <div className='pl-2'>
+                  <Radio group='timer' value='0' isDefault>
+                    {t('limit', { minute: 0 })}
+                  </Radio>
+                  <Radio group='timer' value='60'>
+                    {t('limit', { minute: 1 })}
+                  </Radio>
+                  <Radio group='timer' value='120'>
+                    {t('limit', { minute: 2 })}
+                  </Radio>
+                </div>
               </div>
-            )}
-            <div>
-              <h2>{t('timer')}</h2>
-              <div className='pl-2'>
-                <Radio group='timer' value='0' isDefault>
-                  {t('limit', { minute: 0 })}
-                </Radio>
-                <Radio group='timer' value='60'>
-                  {t('limit', { minute: 1 })}
-                </Radio>
-                <Radio group='timer' value='120'>
-                  {t('limit', { minute: 2 })}
-                </Radio>
+              <div className='max-md:col-span-2'>
+                <input type='checkbox' id='hint' name='hint' />
+                <label className='pl-2' htmlFor='hint'>
+                  {t('area_hint')}
+                </label>
               </div>
-            </div>
-            <div>
-              <input type='checkbox' id='hint' name='hint' />
-              <label className='pl-2' htmlFor='hint'>
-                {t('area_hint')}
-              </label>
-            </div>
-            <div className='mx-auto col-span-2'>
-              <label htmlFor='rounds'>{t('rounds')}</label>
-              <input
-                type='number'
-                id='rounds'
-                name='rounds'
-                defaultValue={5}
-                className='bg-primary flex rounded-xl p-2'
-              />
-            </div>
-            <Button enable={!loading} className='mx-auto col-span-2 mt-2' type='submit'>
-              {t('create')}
-            </Button>
-            {error && <ErrorCard className='col-span-2'>{error}</ErrorCard>}
-          </form>
+              <div className='mx-auto col-span-2'>
+                <label htmlFor='rounds'>{t('rounds')}</label>
+                <input
+                  type='number'
+                  id='rounds'
+                  name='rounds'
+                  defaultValue={5}
+                  className='bg-primary flex rounded-xl p-2'
+                />
+              </div>
+              <Button enable={!loading} className='mx-auto col-span-2 mt-2' type='submit'>
+                {t('create')}
+              </Button>
+              {error && <ErrorCard className='col-span-2'>{error}</ErrorCard>}
+            </form>
+          </div>
         </div>
       </div>
     </main>
