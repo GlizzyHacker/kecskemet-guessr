aef8bf112c167268cc60381fa9a8b17fe465839a
Attila Sax <75539035+GlizzyHacker@users.noreply.github.com>
2025-09-30 22:04:14 +0200

    Update request.ts

--- apps/frontend/src/i18n/request.ts
+++ apps/frontend/src/i18n/request.ts
@@ -4,8 +4,8 @@ import { cookies, headers } from "next/headers";
 const supportedLocales = ["en", "hu"];
 
 export default getRequestConfig(async () => {
-  const acceptedLocales = (await headers()).get("accept-language")?.split(",");
-  const locale = (await cookies()).get("NEXT_LOCALE")?.value || acceptedLocales?.filter((locale) => supportedLocales.includes(locale.split(";")[0]))[0] || "en";
+  const acceptedLocales = (await headers()).get("accept-language")?.split(",").map((locale) => locale.split(";")[0]);
+  const locale = (await cookies()).get("NEXT_LOCALE")?.value || acceptedLocales?.filter((locale) => supportedLocales.includes(locale))[0] || "en";
 
   return {
     locale,
