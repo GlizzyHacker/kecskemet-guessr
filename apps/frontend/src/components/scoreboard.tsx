82d45f3a8078272a5af9e52b74d8976f70472629
Attila Sax <saxattila@gmail.com>
2025-10-31 19:43:44 +0100

    added framer motion animation

--- apps/frontend/src/components/scoreboard.tsx
+++ apps/frontend/src/components/scoreboard.tsx
@@ -1,4 +1,5 @@
 import { Guess, Member, Round } from '@/types/game';
+import { motion } from 'framer-motion';
 import { useTranslations } from 'next-intl';
 import Card from './card';
 import ScoreboardItem from './scoreboard_item';
@@ -29,17 +30,17 @@ export default function Scoreboard({
         <p className='col-span-2 bg-surface-container-low text-center p-2 overflow-ellipsis'>{t('name')}</p>
         <p className='col-span-2 bg-surface-container-low text-center p-2 overflow-ellipsis'>{t('score')}</p>
         <p className='bg-surface-container-low text-center p-2 overflow-hidden'>{t('guessed')}</p>
-        <div className='col-span-6'>
-          {sorted?.map((member, i) => [
+        <motion.ul layout className='col-span-6 bg-surface-container-low'>
+          {sorted?.map((member, i) => (
             <ScoreboardItem
               key={member.id}
               member={member}
               placement={i}
               currentRound={currentRound}
               onKick={onKick == undefined ? undefined : async () => onKick(member)}
-            />,
-          ])}
-        </div>
+            />
+          ))}
+        </motion.ul>
       </div>
     </Card>
   );
